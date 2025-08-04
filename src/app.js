da88f0bb5fd695d4d4ccc974d8acfeb447ed6442
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-08-05 05:17:58 +0530

    Refactor CORS configuration in app.js to use default settings, simplifying the setup by removing specific origins.

--- src/app.js
+++ src/app.js
@@ -14,16 +14,21 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://sgdevstudio.in",
-      "https://www.sgdevstudio.in",
-    ],
-    credentials: true,
-  })
-);
+const corsOptions =
+  process.env.APP_ENV === "development"
+    ? {
+        origin: true,
+      }
+    : {
+        origin: [
+          "http://localhost:3000",
+          "https://sgdevstudio.in",
+          "https://www.sgdevstudio.in",
+        ],
+        credentials: true,
+      };
+
+app.use(cors(corsOptions));
 
 // Mount your routes
 app.use(routes);
